03a0cfae9f176563084ef560c463220e17f18a8d
marius <mariusdevisevici@yahoo.ro>
2026-04-16 13:27:12 +0300

    db init

---
+++ lib/prisma.ts
@@ -0,0 +1,24 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set. Add your Neon pooled URL to .env");
+}
+
+const adapter = new PrismaNeon({ connectionString });
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
